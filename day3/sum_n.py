ca97e89f1ddd07299d0d9c364413adf3f3e8add3
poorneshwar6-a11y <poorneshwar6@gmail.com>
2026-02-17 20:38:09 +0530

    Rename variable 'sum' to 'total' for clarity

--- day3/sum_n.py
+++ day3/sum_n.py
@@ -3,16 +3,16 @@
 #USING WHILE LOOP
 num=int(input("enter the number to do sum upto it:"))
 i=0
-sum=0
+total=0
 while(i<=num):
-    sum=sum+i
+    total+=i
     i=i+1
-print("sum is",sum)
+print("sum is",total)
 
 
 #USING FOR LOOP
 num=int(input("enter the number to do sum upto it:"))
-sum=0
+total=0
 for i in range(num+1):
-    sum=sum+i
-print("sum is:",sum)
+    total=total+i
+print("sum is:",total)
